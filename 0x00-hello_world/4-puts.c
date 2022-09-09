eb44375d69b15dc3ad60450870072164d1c05632
Fecochuks <bboyfeco13@gmail.com>
2022-09-08 18:26:18 -0700

    Task 4 commit programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
 * main - print the string in the put function
-*
+* 
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* return: 0
+* Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
