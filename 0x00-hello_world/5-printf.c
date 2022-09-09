e33dc001a109f21eaa18cb1b3ff059230bce26de
Fecochuks <bboyfeco13@gmail.com>
2022-09-08 19:29:11 -0700

    Task 5 commit programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 include <stdio.h>                                                                                                           
                                                                                                                             
 /**                                                                                                                         
-* main - print the string in the put function                                                                               
-*                                                                                                                           
-* Description: using the main function                                                                                      
-* this program prints "Programming is like building a multilingual puzzle                                                   
-* Return: 0                                                                                                                 
+* Description: main - prints out a stringto stdout.                                                 
+* Return: 0 if success.                                                                              
 */                                                                                                                          
 int main(void)                                                                                                              
-{                                                                                                                           
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                
-        return (0);                                                                                                         
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
