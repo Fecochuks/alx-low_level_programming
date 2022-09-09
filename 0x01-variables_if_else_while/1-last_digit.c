2a9060b02980ae091811c7a6b4c7ef99a54a08f3
Fecochuks <bboyfeco13@gmail.com>
2022-09-09 16:28:51 -0700

    Task 1 commit variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,24 @@
 */
 int main(void)
 {
-int n;
-int l;
+	int n;
+	char last[] = "Last digit of;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-l = n % 10;
+printf("%s %d is %d and is", last, n, n % 10);
 
-if (l > 5)
+if (n % 10 > 5)
 {
-	printf("Last digit of %d and is greater than 5\n", n, l);
+	printf("greater than 5\n");
 }
-else if (l == 0)
+else if (n % 10 == 0)
 {
-	printf("Last digit of %d is %d and is 0\n", n, l);
+	printf("0\n");
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	printf("less than 6 and not 0\n");
 }
 return (0);
 }
