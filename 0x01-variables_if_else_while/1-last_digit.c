86fce8ef2c1dd034034f0958229f64a4feb9762d
Fecochuks <bboyfeco13@gmail.com>
2022-09-09 15:45:01 -0700

    Task 1 commit variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,32 +3,32 @@
 #include <stdio.h>
 
 /**
-* main - print if the number positive, zero, or negative
+* main - print the last digit of the number stored in the variable
 *
 * Description: using the main function
-* this program prints "Programming is positive, zero, or negative
+* this program prints "Print the last digit of the number stored in the variable
 * Return: 0
 */
 int main(void)
 {
 int n;
-int 1;
+int L;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-1 = n % 10;
+L = n % 10;
 
-if (1 > 5)
+if (L > 5)
 {
-	printf("Last digit of %d and is greater than 5\n", n, 1);
+	printf("Last digit of %d and is greater than 5\n", n, L);
 }
-else if (1 == 0)
+else if (L == 0)
 {
-	printf("Last digit of %d is %d and is 0\n", n, 1);
+	printf("Last digit of %d is %d and is 0\n", n, L);
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, L);
 }
 return (0);
 }
