1a44e38c4d8e4242a8524b195b35e9125a2da6a1
Fecochuks <bboyfeco13@gmail.com>
2022-09-09 18:16:20 -0700

    Task 1 commit variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
 * main - Entry point
 *
@@ -9,23 +9,19 @@
 int main(void)
 {
 	int n;
-	char last[] = "Last digit of;
+	int L;
+	char str[] = "Last digit of";
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("%s %d is %d and is", last, n, n % 10);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
 
-if (n % 10 > 5)
-{
-	printf("greater than 5\n");
-}
-else if (n % 10 == 0)
-{
-	printf("0\n");
-}
-else
-{
-	printf("less than 6 and not 0\n");
-}
-return (0);
+	if (L > 5)
+	printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+	printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+	printf("%s %d is %d and is less tahn 6 and not 0\n", str, n, L);
+
+	return (0);
 }
