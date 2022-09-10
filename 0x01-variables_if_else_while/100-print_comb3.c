28baa686b7ce6350db1e93bd30126bc03d8248f4
Fecochuks <bboyfeco13@gmail.com>
2022-09-09 19:15:45 -0700

    Task 10 commit variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,16 @@
 int main(void)
 {
 	int d = 0, d2;
-	while(d <= 9)
+while (d <= 9)
 	{
 	d2 = 0;
-	while(d2 <= 9)
+	while (d2 <= 9)
 	{
-	if(d != d2 && d < d2)
+	if (d != d2 && d < d2)
 	{
 	putchar(d + 48);
 	putchar(d2 + 48);
-
-	if(d + d2 != 17)
+	if (d + d2 != 17)
 	{
 	putchar(',');
 	putchar(' ');
