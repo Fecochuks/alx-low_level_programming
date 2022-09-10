4de17826235a058b1d5156111d25b12883b3d6ad
Fecochuks <bboyfeco13@gmail.com>
2022-09-09 19:59:32 -0700

    Task 12 commit variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d = 0, d2;
+while (d <= 99)
+	{
+	d2 = d;
+	while (d2 = 99)
+	{
+	if (d2 != d)
+	{
+	putchar((d / 10) + 48);
+	putchar((d % 10) + 48);
+	putchar(' ');
+	putchar((d2 / 10) + 48);
+	putchar((d2 % 10) + 48);
+	if (d != 98 || d2 != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	++d2;
+	}
+	++d;
+	}
+	putchar('\n\);
+
+	return (0);
+}
