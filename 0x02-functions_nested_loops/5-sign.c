8a71b651d549359f4e8f19c094f31c041857a8ca
Fecochuks <bboyfeco13@gmail.com>
2022-09-13 16:23:43 -0700

    Task 5 commit functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,13 +4,13 @@
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
-int print_sign(int; n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 	_putchar('+');
 	return (1);
-	}
+}
 	else if (n < 0)
 	{
 	_putchar('-');
