070916bc468f2ea8f31c15691f9897000bff732b
Fecochuks <bboyfeco13@gmail.com>
2022-09-13 15:30:45 -0700

    Task 2 commit functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,18 @@
  *
  * Return: 0
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char c, i;
 
 	for (i = 0; i <=9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-	{
+		{
 		_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 
 	return (0);
 }
