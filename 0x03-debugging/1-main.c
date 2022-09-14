edf7e6dd9406c4313250465dbcb33231d7d1a4a9
Fecochuks <bboyfeco13@gmail.com>
2022-09-14 09:58:31 -0700

    Task 1 commit debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,18 +7,20 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinte loop incoming :(\n");
+	printf("Infinte loop incoming :(\n");
 
-i = 0;
-/*
-*while (i < 10)
-*{
-*putchar(i);
-*}
+	i = 0;
+
+	while (i < 10)
+/**
+ * {
+ * 	putchar(i);
+ * }
 */
-printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
