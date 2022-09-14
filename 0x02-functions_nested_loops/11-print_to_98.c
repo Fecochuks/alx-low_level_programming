6e100204a5b77586e3d4f12b2efed0185526c24d
Fecochuks <bboyfeco13@gmail.com>
2022-09-13 18:14:55 -0700

    Task 11 commit functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print to 98 - prints all natural numbers from input to 98.
+ * print_to_98 - prints all natural numbers from input to 98.
  * in order seperated by a comma followed by a space.
  * @n: The number to begin counting at.
 */
