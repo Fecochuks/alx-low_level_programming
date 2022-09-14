6bda9349e4002e13366c29f5df679daafa8827ef
Fecochuks <bboyfeco13@gmail.com>
2022-09-14 11:49:29 -0700

    Task 14 commit functions

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ *
+ * Numbers must be separated by comma and space
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int count;
+	long int i = 1, j = 2, k;
+
+	printf("%ld, ", i);
+
+	for (count = 2; count <= 50; count++)
+	{
+		if (count == 50)
+			printf("%ld\n", j);
+		else
+
+			printf("%ld, ", j);
+
+		k = j;
+		j += i:
+		i = k;
+	}
+
+	return (0);
+}
