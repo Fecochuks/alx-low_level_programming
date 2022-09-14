75ddef8d9f45db5702b133429bd5b56453a747c5
Fecochuks <bboyfeco13@gmail.com>
2022-09-14 11:15:21 -0700

    Task 12 commit functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times_table - Print the'n' times table starting with 0
+ * print_times_table - Print the 'n' times table starting with 0
  *
  * Description: if 'n' is greater than 15 or less than 0, print nothing
  * @n: int type number
@@ -10,32 +10,33 @@ void print_times_table(int n)
 	int x, y, z;
 
 	if (n > 15 || n < 0)
-	return;
+		return;
 	for (x = 0; x <= n; x++)
 	{
-	for (y = 0; y <= n; y++)
-	{
-	z = x * y;
-	if (z > 99)
-	{
-	_putchar((z / 100) + '0');
-	_putchar(((z / 10) % 10) + '0');
-	_putchar((z % 10) + '0');
-	}
-	else if (y != 0)
-	{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(z + '0');
-	}
-	else
-	_putchar(z + '0');
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar((z / 100) + '0');
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
 
-	if (y != n)
-	{
-	_putchar(',');
-	_putchar(' ');
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
