e57be06ea522a0910cf78ca63f6bc267bc35cf27
Fecochuks <bboyfeco13@gmail.com>
2022-09-15 13:29:34 -0700

    Task 8 commit more functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,17 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
-
-		if (i != size - 1)
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
