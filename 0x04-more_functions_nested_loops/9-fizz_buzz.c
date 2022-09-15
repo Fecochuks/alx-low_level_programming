e9a7f4d09a0b04bd16f89e5a6f5f5cfeb9f796e3
Fecochuks <bboyfeco13@gmail.com>
2022-09-15 15:13:50 -0700

    Task 9 commit more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - program that printsthe numbers from 1 to 100.
+*
+* PrintFizz if divisble by 3 and Buzz if divisible by 5.
+* Each number or word should be separated by a space.
+* You are allowed to use the standard library.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
