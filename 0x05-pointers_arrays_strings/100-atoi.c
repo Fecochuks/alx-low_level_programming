31c88d634be0d9924087a20e3e084eea9a3c7b65
Fecochuks <bboyfeco13@gmail.com>
2022-09-19 08:04:21 -0700

    Task 10 commit pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,8 +12,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 
@@ -22,9 +21,7 @@ int _atoi(char *s)
 
 		else if (num > 0)
 			break;
-
-	}
-	while (*s++);
+		} while (*s++);
 	
 	return (num * sign);
 }
