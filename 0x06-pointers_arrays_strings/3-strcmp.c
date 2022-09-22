3374c153bba327a6cd6b5208e26f40883eed9432
Fecochuks <bboyfeco13@gmail.com>
2022-09-22 07:04:08 -0700

    Task 3 commit more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-			retrn (s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 	}
-	retrun (0);
+	return (0);
 }
