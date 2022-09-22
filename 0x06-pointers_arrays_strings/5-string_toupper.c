fc85da6ee30af2b42139becadc5c2d4350b09c0a
Fecochuks <bboyfeco13@gmail.com>
2022-09-22 10:59:33 -0700

    Task 5 commit more pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @n: string to be changed
+ *
+ * Return: capitalized string
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\o'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
