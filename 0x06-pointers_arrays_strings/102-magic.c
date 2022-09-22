978ada4ebd08fec0940160c114737e8810bfad2c
Fecochuks <bboyfeco13@gmail.com>
2022-09-22 12:27:54 -0700

    Task 10 commit more pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 int main(void)
 {
@@ -19,6 +18,7 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n", a[2];
 			return (0);
 }
+
