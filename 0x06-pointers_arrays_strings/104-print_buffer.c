28e8f524154ccab79e7d63c695bfe03e1370d660
Fecochuks <bboyfeco13@gmail.com>
2022-09-23 09:36:32 -0700

    Task 12 commit more pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,4 @@
-/**
+/*
  * File: 104-print_buffer.c
  * Auth: Chukwu ifeanyichukwu
  */
@@ -8,8 +8,8 @@
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *                the byte position, then showing the hex content,
- *                then displaying printable characters.
+ * the byte position, then showing the hex content,
+ * then displaying printable characters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
@@ -36,7 +36,7 @@ void print_buffer(char *b, int size)
 			if ((index + byte) >= size)
 				break;
 			else if (*(b + index + byte) >= 31 &&
-                                 *(b + index + byte) <= 126)
+				*(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
 			else
 				printf(".");
