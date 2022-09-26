8929e2ace9c2d785e67739295d335c8f3b743570
Fecochuks <bboyfeco13@gmail.com>
2022-09-26 15:19:49 -0700

    Task 2 commit even more pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _strchr - locates a character in a string
+ *_strchr - locates a character in a string
  *
- * @s: pointer that contains the address of the string
- * @c: character of the first occurrence of c in the string
+ *@s: pointer that contains the address of the string
+ *@c: character that needs to be located with in the string
  *
- * Return: address of the first occurrence of c in the string
+ *Return: address of the first occurrence of c in the string
  */
 
-char *strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	char *i = s;
 
