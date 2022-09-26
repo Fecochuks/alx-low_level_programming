07ae3080ddd2fb25c289919c754dbd26ce7f1b45
Fecochuks <bboyfeco13@gmail.com>
2022-09-26 14:03:34 -0700

    Task 4 commit Even more pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strpbrk - searches a string for any of a set of bytes
+ *
+ *@s: pointer that contains the address of the string to be evaluated
+ *@accept: pointer that contains the string to evaluate s
+ *
+ *Return: address of the first occurrence of accept in the string
+ */
+char *strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != 0)
+	{
+		for (i = 0; accept[i] != 0; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+}
