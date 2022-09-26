a1bbea6e721630cd8da4c8eb5ee079a4ffd7cac8
Fecochuks <bboyfeco13@gmail.com>
2022-09-26 13:30:29 -0700

    Task 1 commit Even more pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area from src to dest n amount of times
+ *
+ * @dest: pointer that will recieve the copied data
+ * @src: pointer that has the information to be copied
+ * @n: amount of times the src information will be copied to dest
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
