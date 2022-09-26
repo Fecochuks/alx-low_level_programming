53ea68173041b03fc72a2c528e7db5c4f5d7c69e
Fecochuks <bboyfeco13@gmail.com>
2022-09-26 14:19:31 -0700

    Task 5 commit Even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ *_strstr - locates a substring within a string
+ *
+ *@haystack: pointer that contains the address of the string to be evaluated
+ *@needle: pointer that contains the string to evaluate @haystack
+ *
+ *Return: address of the located string in @haystack if no substring is found
+ *then return NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		a = i;
+		while (needle[j] != 0)
+		{
+			if  (needle[j] == haystack[a])
+			{
+				compare = 1;
+			}
+			else
+			{
+				compare = 0;
+				break;
+			}
+			j++;
+			a++;
+		}
+		if (compare == 1)
+		{
+			return (haystack + i);
+		}
+		i++;
+	}
+	return (0);
+}
