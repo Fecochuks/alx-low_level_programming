43deef0d33bc95426426e92a06460fc164c148b5
Fecochuks <bboyfeco13@gmail.com>
2022-09-27 00:15:30 -0700

    Task 6 commit even more pointers hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_chessboard - prints the chessboard
- * @a: pointer to pieces to print
- *
- * Return: void
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 
 void print_chessboard(char(*a)[8])
 {
-	int i, j;
+	int i, n;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
 		_putchar('\n');
 	}
 }
