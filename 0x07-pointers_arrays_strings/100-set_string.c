796e7e86ed28e0ad7852dd00b994d6bc16741d41
Fecochuks <bboyfeco13@gmail.com>
2022-09-26 17:40:55 -0700

    Task 8 commit even more pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: string to set
- * @to: value to be set to
- *
- * Return: void
+ * set_string - Sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
  */
 void set_string(char **s, char *to)
 {
