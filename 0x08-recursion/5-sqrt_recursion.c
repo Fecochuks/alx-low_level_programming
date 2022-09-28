6be21f35da057221fc5188fc4be20caea3822085
Fecochuks <bboyfeco13@gmail.com>
2022-09-28 01:30:58 -0700

    Task 5 commit recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * __sqrt_recursion - returns the natural square root of a number
+ * @n: integer to compute square root of
+ * @i: iterate number
+ * Return: integer square root
+ */
+
+int __sqrt_recursion(int n, int i)
+{
+	if (i * i < n)
+		return (__sqrt_recursion(n, i + 1));
+	return (i * i == n ? i : -1);
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number 
+ * @n: integer to compute square root of
+ *
+ * Return: integer square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (__sqrt_recursion(n, 0));
+}
