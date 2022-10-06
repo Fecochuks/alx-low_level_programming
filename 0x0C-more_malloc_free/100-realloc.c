dc5cf19606de6c7ee7a46aa31e460aff55a64f27
Fecochuks <bboyfeco13@gmail.com>
2022-10-06 05:32:52 -0700

    Task 4 commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 {
-	char *clone, *realloc;
+	char *clone, *relloc;
 	unsigned int i;
 
 	if (ptr != NULL)
@@ -22,12 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 	{ free(ptr);
 	return (0); }
 	realloc = malloc(new_size);
-	if (realloc == NULL)
+	if (relloc == NULL)
 	return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(realloc + i) = clone[i];
+		*(relloc + i) = clone[i];
 	}
 	free(ptr);
-return (realloc);
+return (relloc);
 }
